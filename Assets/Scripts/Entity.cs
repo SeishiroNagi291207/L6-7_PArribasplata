004312bc7c7bf30cb0e7f1510e13f1f28e145d82
SeishiroNagi291207 <pieroac2912@gmail.com>
2025-10-05 14:24:17 -0500

    Update Entity.cs

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -1,16 +1,7 @@
 using UnityEngine;
 
-public class Entity : MonoBehaviour
+public abstract class Entity : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public int ID;
+    public string EntityName;
 }
